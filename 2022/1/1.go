fb78302dfd4ca7886b8fd670b1026479f31fb6dc
francoisgognet <fgognet@gmail.com>
2022-12-03 21:35:40 +0100

    init

---
+++ 2022/1/1.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	readFile, err := os.Open("1.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	readFile.Close()
+}
