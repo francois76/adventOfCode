607b8e3a084e4e6b0ffc9c2db7bc9bf7535bcbc6
francoisgognet <fgognet@gmail.com>
2022-12-05 21:31:35 +0100

    5th december 2022 b solution

--- 2022/5b/5b.go
+++ 2022/5b/5b.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	shared.Run(func() interface{} {
 		stacks := map[int64][]string{}
-		shared.Open("5_init.txt", func(fileScanner *bufio.Scanner) {
+		shared.Open("../5/5_init.txt", func(fileScanner *bufio.Scanner) {
 			rawLine := fileScanner.Text()
 			line := strings.ReplaceAll(rawLine, "]    ", "] @")
 			line = strings.ReplaceAll(line, "    [", "@ [")
@@ -28,16 +28,14 @@ func main() {
 				stacks[int64(idx+1)] = append([]string{item}, stacks[int64(idx+1)]...)
 			}
 		})
-		shared.Open("5_steps.txt", func(fileScanner *bufio.Scanner) {
+		shared.Open("../5/5_steps.txt", func(fileScanner *bufio.Scanner) {
 			lineSplit := strings.Split(fileScanner.Text(), " ")
 			number, _ := strconv.ParseInt(lineSplit[1], 10, 64)
 			from, _ := strconv.ParseInt(lineSplit[3], 10, 64)
 			to, _ := strconv.ParseInt(lineSplit[5], 10, 64)
 			payload := stacks[from][len(stacks[from])-int(number):]
 			stacks[from] = stacks[from][:len(stacks[from])-int(number)]
-			for idx := number - 1; idx >= 0; idx-- {
-				stacks[to] = append(stacks[to], payload[idx])
-			}
+			stacks[to] = append(stacks[to], payload...)
 		})
 		for idx := int64(1); idx <= int64(9); idx++ {
 			fmt.Print(string(stacks[idx][len(stacks[idx])-1][1]))
