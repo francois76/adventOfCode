359d53e716e7015c880bbe3d85cd2bb04ed9ae34
francoisgognet <fgognet@gmail.com>
2022-12-07 18:24:08 +0100

    6th december 2022 solution

---
+++ 2022/6/6.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"strings"
+
+	"github.com/francois76/adventOfCode/shared"
+)
+
+func main() {
+
+	shared.Open("6.txt", func(fileScanner *bufio.Scanner) {
+		characters := strings.Split(fileScanner.Text(), "")
+		for idx := 0; idx < len(characters)-4; idx++ {
+			currentFrame := characters[idx : idx+4]
+			set := map[string]bool{}
+			for _, letter := range currentFrame {
+				set[letter] = true
+			}
+			fmt.Println(set)
+			if len(set) == 4 {
+				fmt.Println(idx + 4)
+				return
+			}
+		}
+	})
+}
