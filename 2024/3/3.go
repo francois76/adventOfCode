fe208b5941fc8a96a7f31717a951a1fd61e1eca6
francoisgognet <fgognet@gmail.com>
2024-12-08 19:40:32 +0100

    3 decembre 2024 solution

---
+++ 2024/3/3.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bufio"
+	"regexp"
+	"strconv"
+
+	"github.com/francois76/adventOfCode/shared"
+)
+
+var re = regexp.MustCompile(`(?m)mul\((\d+),(\d+)\)`)
+
+func main() {
+	parseInt := func(s string) int64 {
+		i, _ := strconv.ParseInt(s, 10, 64)
+		return i
+	}
+	shared.Run(func() interface{} {
+		total := int64(0)
+		shared.Open("3.txt", func(fileScanner *bufio.Scanner) {
+			for _, match := range re.FindAllStringSubmatch(fileScanner.Text(), -1) {
+				total += parseInt(match[1]) * parseInt(match[2])
+			}
+		})
+
+		return total
+	})
+
+}
